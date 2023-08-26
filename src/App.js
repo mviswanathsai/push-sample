eec68512bb6b8bfa236de5a5f601fd438bd6578d
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-08-26 23:14:51 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
         let sw = await navigator.serviceWorker.ready;
         let push = await sw.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey:'BKUc_OvdGM4KqyMIt9Cx6oFzkyz7U1pRV01Ykhrp0q25EQulA2d5vmjhCWk3f2twLJ8Q89awxUEobuS30cBh-7U'
+            applicationServerKey: 'BKUc_OvdGM4KqyMIt9Cx6oFzkyz7U1pRV01Ykhrp0q25EQulA2d5vmjhCWk3f2twLJ8Q89awxUEobuS30cBh-7U'
         })
-        console.log(JSON.stringify(push))
+        console.log(push)
     }
 
     return (
